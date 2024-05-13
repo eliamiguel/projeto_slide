82311913fce9381a13437bae63f74ce94183b285
Elias Perreira Miguel <eliasperreiramiguel@gmail.com>
2024-05-13 18:35:09 -0300

    contagem

--- mian.js
+++ mian.js
@@ -1,6 +1,7 @@
 
 const containerItens = document.querySelector("#container-Item")
-
+const containerSlides = document.querySelector('.container-slides')
+let id = document.querySelector('#id')
 
 
 const image= [
@@ -12,94 +13,32 @@ const image= [
   {'id': '6','url': './img/yuyuhakusho.jpg'}
 ]
 
-
-
-const carregarImagens = (image, containerItens)=>{
-  image.forEach(img =>{
-    containerItens.innerHTML += `<div class="item">
-    <img src='${img.url}'>
+const laondImagem = (image, containerItens)=>{
+    image.forEach(img =>{
+      containerItens.innerHTML += `
+    <div class="item"> <img src="${img.url}">
+    <span class='ids'>${img.id}</span> 
     </div>
+    
     `
-  })
+  });
 }
 
-
-carregarImagens(image, containerItens)
-
-
+laondImagem(image, containerItens)
 let itens = document.querySelectorAll('.item')
-
-
-const voltar= ()=>{
+console.log()
+const voltar = ()=>{
   containerItens.appendChild(itens[0])
   itens = document.querySelectorAll('.item')
+  id.innerHTML = document.querySelector('.ids').innerText
 }
-
-const proximo= ()=>{
-  const lastItem = itens[itens.length -1]
+const proximo = ()=>{
+  const lastItem = itens[itens.length - 1]
   containerItens.insertBefore(lastItem, itens[0])
   itens = document.querySelectorAll('.item')
+  id.innerHTML = document.querySelector('.ids').innerText
 }
-
-document.querySelector('#voltar').addEventListener('click', voltar)
-document.querySelector('#proximo').addEventListener('click', proximo)
-
-/*
-const carregarImagens = (image, containerItens)=>{
-image.forEach(image =>{
-  containerItens.innerHTML +=`
-<div class='item'> 
-<img src='${image.url}'> 
-</div>
-`});
-}
-
-
-carregarImagens(image, containerItens)
-
-
 document.querySelector("#voltar").addEventListener("click", voltar)
 document.querySelector("#proximo").addEventListener("click", proximo)
-let item = document.querySelectorAll('.item')
 
 
-
-function voltar (){
-  containerItens.appendChild(item[0])
-  item = document.querySelectorAll(".item")
-}
-function proximo() {
-  const lastItem = item[item.length -1]
-  containerItens.insertBefore(lastItem, item[0]) 
-  item = document.querySelectorAll(".item")
-  
-}
-
-*/
-
-
-/*const carregar= (img,containerItens )=>{ 
-   img.forEach(image => {
-    containerItens.innerHTML += `<div class="item">
-   <img src="${image.url}">
-   </div>`
-  })
-}
-carregar(img,containerItens)
-let item = document.querySelectorAll(".item")
-document.querySelector("#voltar").addEventListener('click', voltar)
-document.querySelector("#proximo").addEventListener('click', next)
-
-function voltar() {
-  containerItens.appendChild(item[0])
-  item = document.querySelectorAll(".item")
- 
-}
-
-function next() {
-  const lastItem= item[item.length -1]
-  containerItens.insertBefore(lastItem, item[0])
-  item = document.querySelectorAll(".item")
-}
-
-*/
